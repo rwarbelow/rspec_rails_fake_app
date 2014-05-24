cce048d94ca68b701d1d732f1748e0f91d2fa39c
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-05-24 13:42:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RspecRailsFakeApp::Application.config.secret_key_base = 'e8dd267bbff594cafd83b03177f9a6d37740351059d325a823738ef414d3e57706cccd03f04e4356fc3346cdf5c6a7cd40339983e37046e44a58830e36186531'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RspecRailsFakeApp::Application.config.session_store :cookie_store, key: '_rspec_rails_fake_app_session'

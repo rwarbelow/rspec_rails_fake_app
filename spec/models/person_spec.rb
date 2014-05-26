c60a1f1a119f9c97320b41a5f12b6edec0dffefc
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-05-25 23:22:51 -0700

    refactored person spec for succinctness

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 
 describe Person do
+	let(:person) { Person.new(first_name: "Rachel", last_name: "Warbelow") }
+	
   it 'is valid' do
-  	person = Person.new(first_name: "Rachel", last_name: "Warbelow")
   	expect(person).to be_valid
   end
 
   it 'is invalid without a first name' do
-  	person = Person.new(first_name: nil, last_name: "Warbelow")
+  	person.first_name = nil
   	expect(person).not_to be_valid
   end
 
   it 'is invalid without a last name' do
-  	person = Person.new(first_name: "Henry", last_name: nil)
+  	person.last_name = nil
   	expect(person).not_to be_valid
   end
 
